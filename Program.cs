c1a0dd0d44eff5719960832f90f621c7f249e25f
Alex Paige <apaige161@gmail.com>
2020-08-13 21:06:25 -0400

    initial

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using static System.Console;
+
+namespace tryParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many eggs are there?");
+            int count;
+            string input = Console.ReadLine();
+            if(int.TryParse(input, out count))
+            {
+                Console.WriteLine($"There are {count} eggs");
+            }
+            else 
+            {
+                Console.WriteLine("Could not parse the input");
+            } 
+
+            WriteLine("before parsing");
+
+            Write("What is your age?");
+            string input1 = ReadLine();
+
+            try
+            {
+                int age = int.Parse(input1);
+                WriteLine($"You are {input1} years old");
+            }
+            catch(OverflowException)
+            {
+                WriteLine("The input was a number but it was either too large or too small");
+            }
+            catch(FormatException)
+            {
+                WriteLine("The age you entered is not a valid number");
+            }
+            catch(Exception ex)
+            {
+                WriteLine($"{ex.GetType()} says {ex.Message}");
+            }
+            Write("After parsing");
+        }
+    }
+}
